c2127a8fcf9a79f868ecbc2691a1a49b1ef1b32e
zbwbb <zhoubinxinna@sina.com>
2019-07-16 09:26:03 +0800

    first submit

---
+++ Example/Pods/Target Support Files/HKCategoryKit/HKCategoryKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HKCategoryKit : NSObject
+@end
+@implementation PodsDummy_HKCategoryKit
+@end
---
+++ Example/Pods/Target Support Files/Pods-HKCategoryKit_Tests/Pods-HKCategoryKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HKCategoryKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_HKCategoryKit_Tests
+@end
